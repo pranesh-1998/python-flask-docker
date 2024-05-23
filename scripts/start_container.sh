8f93477442e5735b0bf49431b1b94795862a888e
pranesh-1998 <72786460+pranesh-1998@users.noreply.github.com>
2024-05-23 11:10:55 +0530

    Create start_container.sh

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull pranesh1998/hotel-app:latest
+
+# Run the Docker image as a container
+docker run -dit -p 80:80 pranesh1998/hotel-app
